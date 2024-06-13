366cb5a9392d6ff250e1148bbcb25a2dcca3a69a
EnatMathews <enatmathews@gmail.com>
2024-06-13 20:18:57 +0530

    project created

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -11,16 +11,11 @@ const SignUp = () => {
 const [data,changedata]=useState(
     {
          "name":"",
-         "dobb":"",
-         "blood":"",
-         "mob":"",
-         "addd":"",
-         "pin":"",
-         "dir":"",
-         "place":"",
          "emaill":"",
-         "user":"",
-         "pass":"",
+         "phoneno":"",
+         "gender":"",
+         
+         "password":"",
          "conf":"",
     }
 )
@@ -70,95 +65,33 @@ const readValue=()=>{
     </div>
     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
-    <label htmlFor="" className="form-label">Date Of Birth</label>
-    <input type="date" name="dobb" id="" className="form-control" value={data.dobb} onChange={inputHandler} />
+    <label htmlFor="" className="form-label">Email ID</label>
+    <input type="text" className="form-control" name="email" value={data.email} onChange={inputHandler} />
+    </div>
+    
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+
+    <label htmlFor="" className="form-label">Phone No</label>
+    <input type="text" className="form-control" name="phoneno" value={data.phoneno} onChange={inputHandler} />
 
     </div>
     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
 
-    <label htmlFor="" className="form-label">BloodGroup</label>
-    <select name="blood" id="" className="form-control" value={data.blood} onChange={inputHandler} >
-        <option value="A+">A+</option>
-        <option value="A-">A-</option>
-        <option value="B+">B+</option>
-        <option value="B-">B-</option>
-        <option value="AB+">AB+</option>
-        <option value="AB-">AB-</option>
-        <option value="O+">O+</option>
-        <option value="O-">O-</option>
-    </select>
-
-    </div>
-    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-
-    <label htmlFor="" className="form-label">Mobile No</label>
-    <input type="text" className="form-control" name="mob" value={data.mob} onChange={inputHandler} />
-
-    </div>
-    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
-
-    <label htmlFor="" className="form-label">Address</label>
-    <textarea name="addd" id="" className="form-control" value={data.addd} onChange={inputHandler} ></textarea>
-
-    </div>
-    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-
-    <label htmlFor="" className="form-label">PinCode</label>
-    <input type="text" className="form-control" name="pin" value={data.pin} onChange={inputHandler} />
-
-    </div>
-    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-
-    <label htmlFor="" className="form-label">District</label>
-    <select name="dir" id="" className="form-control" value={data.dir} onChange={inputHandler} >
-        <option value="Thiruvananthapuram">Thiruvananthapuram</option>
-        <option value="Kollam">Kollam</option>
-        <option value="Pathanamthitta">Pathanamthitta</option>
-        <option value="Alappuzha">Alappuzha</option>
-        <option value="Kottayam">Kottayam</option>
-        <option value="Idukki">Idukki</option>
-        <option value="Ernakulam">Ernakulam</option>
-        <option value="Thrissur">Thrissur</option>
-        <option value="Palakkad">Palakkad</option>
-        <option value="Malappuram">Malappuram</option>
-        <option value="Kozhikode">Kozhikode</option>
-        <option value="Wayanad">Wayanad</option>
-        <option value="Kannur">Kannur</option>
-        <option value="Kasaragod">Kasaragod</option>
-    </select>
-
-    </div>
-    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-
-    <label htmlFor="" className="form-label">Place</label>
-    <input type="text" className="form-control" name="place" value={data.place} onChange={inputHandler} />
-
-    </div>
-    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-
-    <label htmlFor="" className="form-label">EmailId</label>
-    <input type="text" className="form-control" name="emaill" value={data.emaill} onChange={inputHandler} />
-
-    </div>
-    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
-    <label htmlFor="" className="form-label">UserName</label>
-    <input type="text" className="form-control" name="user" value={data.user} onChange={inputHandler} />
-
-
-    </div>
+<label htmlFor="" className="form-label">Gender</label>
+<select name="gender" id="" className="form-control">
+    <option value="">Male</option>
+    <option value="">Female</option>
+    <option value="">Other</option>
+</select>
+</div>
+    
     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
 
     <label htmlFor="" className="form-label">Password</label>
-    <input type="password" name="pass" id="" className="form-control"  value={data.pass} onChange={inputHandler} />
+    <input type="password" name="password" id="" className="form-control"  value={data.password} onChange={inputHandler} />
 
     </div>
     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
