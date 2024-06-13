369d2ff6faa509b175be3dcebf0a5ebf4bc239dc
EnatMathews <enatmathews@gmail.com>
2024-06-13 20:19:51 +0530

    project created

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -105,7 +105,7 @@ const readValue=()=>{
     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
 
-<button className="btn btn-success" onClick={readValue}>SignUp</button>
+<button className="btn btn-success" onClick={readValue}>Register</button>
 
     </div>
 
