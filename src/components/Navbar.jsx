58b041697930ff1d5a092fd43ddb3cd9ba3e8d1f
EnatMathews <enatmathews@gmail.com>
2024-06-13 20:49:54 +0530

    project created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,7 @@ const Navbar = () => {
         <div>
             <nav class="navbar bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">
-                        <img src="https://fisat.ac.in/wp-content/themes/fisat2022/assets/logo.png" alt="Logo" width="100" height="100" class="d-inline-block align-text-top" />
-
-                    
-                        <img src="https://fisat.ac.in/wp-content/themes/fisat2022/assets/testcopy.png" alt="Logo" width="700" height="100" class="d-inline-block align-text-top" />
-
-                    </a>
+                    <b>KSRTC BUS APP</b>
                    
 
                 </div>
